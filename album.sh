ee683bcae35180f647833d403875a33cc1c77af9
Will Pringle <willkantorpringle+1@gmail.com>
2022-04-06 11:09:14 -0400

    Add ffmpeg and jp2a to output ascii album art

--- album.sh
+++ album.sh
@@ -1,4 +1,8 @@
 ARTIST=$(cmus-remote -Q | grep -w albumartist | cut -d" " -f 3-)
 ALBUM=$(cmus-remote -Q | grep -w album | cut -d" " -f 3- | head -1)
+FILE=$(cmus-remote -Q | grep -w file | cut -f 2- -d ' ')
 
-echo $ARTIST $ALBUM
+
+
+echo $ARTIST $ALBUM $FILE
+ffmpeg -i "$FILE"  -hide_banner -loglevel error -f image2pipe - | jp2a - --size=90x45 --colors
